962bbbc66efdd0b73d4ff1d273f52f4dfd912e7f
Kenneth <kenneth.ebue@gmail.com>
2023-05-14 16:54:33 -0700

    Temperature Toggle Button

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const form = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
 const weatherResult = document.querySelector('#weather-result');
+const tempToggle = document.querySelector('#temp-toggle');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const city = searchInput.value.trim();
-  if (city.length === 0) {
-    return;
+let isCelsius = true;
+
+function displayWeather(city, weather, temp, time) {
+  let tempDisplay = parseFloat(temp);
+  if (isCelsius) {
+    tempDisplay = (tempDisplay - 32) * 5 / 9;
   }
+  tempDisplay = tempDisplay.toFixed(1);
+  const tempUnit = isCelsius ? "°C" : "°F";
+  weatherResult.innerHTML = `<p>${city} is ${weather.trim()}</p><p>${tempDisplay} ${tempUnit}</p><p>${time.trim()}</p>`;
+}
+
+function fetchWeather(city) {
   const apiUrl = `https://wttr.in/${city}?format=%C,%t,%T`;
   fetch(apiUrl)
     .then(response => {
@@ -18,10 +26,29 @@ form.addEventListener('submit', (e) => {
     })
     .then(data => {
       const [weather, temp, time] = data.split(',');
-      weatherResult.innerHTML = `<p>${city} is ${weather.trim()}</p><p>${temp.trim()}</p><p>${time.trim()}</p>`;
+      displayWeather(city, weather, temp, time);
     })
     .catch(error => {
       console.error('There was a problem fetching the weather data:', error);
       weatherResult.textContent = 'Sorry, there was a problem fetching the weather data.';
     });
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const city = searchInput.value.trim();
+  if (city.length === 0) {
+    return;
+  }
+  fetchWeather(city);
+});
+
+tempToggle.addEventListener('click', (e) => {
+  isCelsius = !isCelsius;
+  const tempUnit = isCelsius ? "°C" : "°F";
+  tempToggle.textContent = `Toggle to ${tempUnit}`;
+  const tempText = weatherResult.querySelector("p:nth-child(2)");
+  const [tempDisplay, tempUnitDisplay] = tempText.textContent.split(" ");
+  const temp = isCelsius ? (tempDisplay - 32) * 5 / 9 : (tempDisplay * 9 / 5) + 32; // updated conversion formula
+  tempText.textContent = `${temp.toFixed(1)} ${tempUnit}`;
 });
